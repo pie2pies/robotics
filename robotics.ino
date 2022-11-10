14afc56d2d23a7d7ceca99b5d9cd16b426af5b46
pie2pies <110467950+pie2pies@users.noreply.github.com>
2022-11-10 13:54:59 +1100

    thingz

--- robotics.ino
+++ robotics.ino
@@ -1,6 +1,8 @@
 #include <Servo.h>
 Servo esc1;
 Servo esc2;
+//Servo esc3;
+//Servo esc4;
 //bluetooth
 #include <SoftwareSerial.h>
 SoftwareSerial mySoftwareSerial(12,13); // RX | TX
@@ -10,6 +12,8 @@ void setup() {
   // put your setup code here, to run once:
 esc1.attach(3, 1000, 2000);
 esc2.attach(5, 1000, 2000);
+//esc3.attach(?, 1000, 2000);
+//esc4.attach(?, 1000, 2000);
 //bluetooth
  Serial.begin(9600);
     Serial.println("Starting i guess?");
@@ -20,6 +24,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   esc1.writeMicroseconds(1500);
   esc2.writeMicroseconds(1500);
+  //esc3.writeMicroseconds(1500);
+  //esc4.writeMicroseconds(1500);
 //bluetooth
 if (mySoftwareSerial.available()){
       command = mySoftwareSerial.read();
@@ -43,16 +49,24 @@ if (mySoftwareSerial.available()){
 
 void forward(){
 //esc1.attach(3, 1000, 2000);
-//esc2.attach(5, 1000, 2000); dont think its needed might be wrong (likely)
-esc1.writeMicroseconds(1600);
-esc2.writeMicroseconds(1600);
+//esc2.attach(5, 1000 2000);
+//esc3.attach(?, 1000, 2000);
+//esc4attach(?, 1000, 2000); dont think its needed might be wrong (likely)
+  esc1.writeMicroseconds(1600);
+  esc2.writeMicroseconds(1600);
+//esc3.writeMicroseconds(1600);
+//esc4.writeMicroseconds(1600);
 }
 
 void back(){
 //esc1.attach(3, 1000, 2000);
-//esc2.attach(5, 1000, 2000); dont think its needed might be wrong (likely)
-esc1.writeMicroseconds(1400);
-esc2.writeMicroseconds(1400);
+//esc2attach(5, 1000, 2000);
+//esc3attach(? 1000, 2000);
+//esc4attach(? 1000, 2000); dont think its needed might be wrong (likely)
+  esc1.writeMicroseconds(1400);
+  esc2.writeMicroseconds(1400);
+//esc3.writeMicroseconds(1400);
+//esc4.writeMicroseconds(1400);
 }
 
 
