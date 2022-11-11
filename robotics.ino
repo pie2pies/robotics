d398a5cf094eac31816906df101d6740d822ccd7
pie2pies <110467950+pie2pies@users.noreply.github.com>
2022-11-11 13:05:59 +1100

    Add files via upload

--- robotics.ino
+++ robotics.ino
@@ -5,19 +5,19 @@ Servo esc2;
 //Servo esc4;
 //bluetooth
 #include <SoftwareSerial.h>
-SoftwareSerial mySoftwareSerial(12,13); // RX | TX
+SoftwareSerial mySoftwareSerial(12, 13); // RX | TX
 char command;
 
 void setup() {
   // put your setup code here, to run once:
-esc1.attach(3, 1000, 2000);
-esc2.attach(5, 1000, 2000);
-//esc3.attach(9, 1000, 2000);
-//esc4.attach(10, 1000, 2000);
-//bluetooth
- Serial.begin(9600);
-    Serial.println("Starting i guess?");
-    mySoftwareSerial.begin(9600);
+  esc1.attach(3, 1000, 2000);
+  esc2.attach(5, 1000, 2000);
+  //esc3.attach(?, 1000, 2000);
+  //esc4.attach(?, 1000, 2000);
+  //bluetooth
+  Serial.begin(9600);
+  Serial.println("Starting i guess?");
+  mySoftwareSerial.begin(9600);
 }
 
 void loop() {
@@ -26,50 +26,101 @@ void loop() {
   esc2.writeMicroseconds(1500);
   //esc3.writeMicroseconds(1500);
   //esc4.writeMicroseconds(1500);
-//bluetooth
-if (mySoftwareSerial.available()){
-      command = mySoftwareSerial.read();
-      Serial.println(command);
+  //bluetooth
+  if (mySoftwareSerial.available()) {
+    command = mySoftwareSerial.read();
+    Serial.println(command);
 
-      if (command == 'F') {
-        Serial.println("forward");
-        
+    if (command == 'F') {
+      Serial.println("forward");
+      //foward();
+
       if (command == 'B') {
         Serial.println("back");
+        //back();
 
-      if (command == 'L') {
-        Serial.println("left"); 
+        if (command == 'L') {
+          Serial.println("left");
+          //left();
 
-      if (command == 'R') {
-        Serial.println("right"); 
+          if (command == 'R') {
+            Serial.println("right");
+            //right();
 
+            /*    if (command == '?') {
+                  Serial.println("45 diagonal foward left");
+                  //fl(); }
+
+                  if (command == '?') {
+                    Serial.println("45 diagonal foward right");
+                    //fr(); }
+
+                    if (command == '?') {
+                      Serial.println("45 diagonal back right");
+                      //br();}
+
+                      if (command == '?') {
+                        Serial.println("45 diagonal back left");
+                        //bl(); }*/
+          }
+        }
+      }
+    }
+  }
 }
-}
-}}}}
 
-void forward(){
-//esc1.attach(3, 1000, 2000);
-//esc2.attach(5, 1000 2000);
-//esc3.attach(?, 1000, 2000);
-//esc4attach(?, 1000, 2000); dont think its needed might be wrong (likely)
+void forward() {
   esc1.writeMicroseconds(1600);
   esc2.writeMicroseconds(1600);
-//esc3.writeMicroseconds(1600);
-//esc4.writeMicroseconds(1600);
+  //esc3.writeMicroseconds(1600);
+  //esc4.writeMicroseconds(1600);
 }
 
-void back(){
-//esc1.attach(3, 1000, 2000);
-//esc2attach(5, 1000, 2000);
-//esc3attach(? 1000, 2000);
-//esc4attach(? 1000, 2000); dont think its needed might be wrong (likely)
+void back() {
   esc1.writeMicroseconds(1400);
   esc2.writeMicroseconds(1400);
-//esc3.writeMicroseconds(1400);
-//esc4.writeMicroseconds(1400);
+  //esc3.writeMicroseconds(1400);
+  //esc4.writeMicroseconds(1400);
 }
 
+void right() {
+  esc1.writeMicroseconds(1600);
+  esc2.writeMicroseconds(1400);
+  //esc3.writeMicroseconds(1400);
+  //esc4.writeMicroseconds(1600);
+}
 
+void left() {
+  esc1.writeMicroseconds(1400);
+  esc2.writeMicroseconds(1600);
+  //esc3.writeMicroseconds(1600);
+  //esc4.writeMicroseconds(1400);
+}
 
+void fl() {
+  esc1.writeMicroseconds(1500);
+  esc2.writeMicroseconds(1600);
+  //esc3.writeMicroseconds(1600);
+  //esc4.writeMicroseconds(1500);
+}
 
+void fr() {
+  esc1.writeMicroseconds(1600);
+  esc2.writeMicroseconds(1500);
+  //esc3.writeMicroseconds(1500);
+  //esc4.writeMicroseconds(1600);
+}
 
+void bl() {
+  esc1.writeMicroseconds(1400);
+  esc2.writeMicroseconds(1500);
+  //esc3.writeMicroseconds(1500);
+  //esc4.writeMicroseconds(1400);
+}
+
+void br() {
+  esc1.writeMicroseconds(1500);
+  esc2.writeMicroseconds(1400);
+  //esc3.writeMicroseconds(1400);
+  //esc4.writeMicroseconds(1500);
+}
